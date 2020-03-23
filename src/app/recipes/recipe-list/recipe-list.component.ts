9e2fc77dcb113d3ccba36ebf138cd4f53c033ac1
Iurii Maratkanov <youran_88>
2020-03-23 23:29:31 +0300

    Пробую выводить список рецептов

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+
+  recipes: Recipe[] = [
+      new Recipe('Название рецепта', 'Описание рецепта','https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg'),
+    new Recipe('Пицца', 'Описание рецепта пиццы','https://www.citypizza.ru/upload/img/shop/pizza/BORT/resize/4-%D1%81%D0%B5%D0%B7%D0%BE%D0%BD%D0%B0-%D0%B1%D0%BE%D1%80%D1%82-listThumb.jpg')
+  ]; // Наш список из рецептов
   constructor() { }
 
   ngOnInit() {
